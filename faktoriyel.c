9885d068ef8e4dc7c1c9e55698bda56e8187d015
onurbcer <436603@ogr.ktu.edu.tr>
2024-04-09 22:38:12 +0300

    first_commit

--- faktoriyel.c
+++ faktoriyel.c
@@ -3,5 +3,15 @@
 /// Bir tamsayının faktöriyelini hesaplayan program
 
 int main() {
-    
+    int x,y=1, faktoriyel=1;
+    printf("Sayı giriniz: ");
+    scanf("%d", &x);
+
+    while (y<=x) {
+    faktoriyel = faktoriyel*y;
+    y++;
+    }
+
+    printf("Sonuç: %d\n", faktoriyel);
+    return 0;
 }
